ef0c88ea07ce466cbefbe993d73a7ed8c9896bb9
Ron Kurr <kurr@kurron.org>
2017-03-03 11:23:05 -0500

    Adjusted things so we could specify what is needed to create the AL cluster

--- scripts/create-stack.sh
+++ scripts/create-stack.sh
@@ -3,12 +3,14 @@
 # creates a stack in AWS via CloudFromation
 
 STACKNAME=${1:-Weapon-X-Search}
-PROJECTNAME=${2:-Weapon-X}
-VOLUME_SIZE=${3:-10}
-ENVIRONMENT=${4:-development}
-CREATOR=${5:-CloudFormation}
-PURPOSE=${6:-Testing}
-TEMPLATELOCATION=${7:-file://$(pwd)/elasticsearch.yml}
+DOMAIN_NAME=${2:-weapon-x}
+INSTANCE_TYPE=${3:-t2.small.elasticsearch}
+VOLUME_SIZE=${4:-10}
+PROJECTNAME=${5:-Weapon-X}
+ENVIRONMENT=${6:-development}
+CREATOR=${7:-CloudFormation}
+PURPOSE=${8:-Testing}
+TEMPLATELOCATION=${8:-file://$(pwd)/elasticsearch.yml}
 
 VALIDATE="aws cloudformation validate-template --template-body $TEMPLATELOCATION"
 echo $VALIDATE
@@ -22,9 +24,11 @@ CREATE="aws cloudformation create-stack --stack-name $STACKNAME \
                                                      ParameterKey=Environment,ParameterValue=$ENVIRONMENT \
                                                      ParameterKey=Creator,ParameterValue=$CREATOR \
                                                      ParameterKey=VolumeSize,ParameterValue=$VOLUME_SIZE \
+                                                     ParameterKey=InstanceType,ParameterValue=$INSTANCE_TYPE \
+                                                     ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME \
                                         --tags Key=Project,Value=$PROJECTNAME \
                                                Key=Purpose,Value=$PURPOSE \
                                                Key=Environment,Value=$ENVIRONMENT \
                                                Key=Creator,Value=$CREATOR"
 echo $CREATE
-#$CREATE
+$CREATE
